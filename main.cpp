e4169cf94dc6eb51a88cbabb5064ca023201c9af
Mateusz Gorzelak <matgorzelak@gmail.com>
2017-04-15 20:01:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ Each digit must be separated by space.
 
 Example:
 
-4 1 3 // a square matrix 4x4, start point = 1, sought point = 3
+4 1 3 // A square matrix 4x4, start point = 1, sought point = 3
 0 1 0 1
 0 0 0 0
 0 0 0 0
