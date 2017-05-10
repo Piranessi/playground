ebd1285f727402e5ac97402073b2a5872380850e
Mateusz Gorzelak <matgorzelak@gmail.com>
2017-05-10 20:35:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,7 @@
 #include "CRUD.h"
-#include <list>
-#include <memory>
-
-class A
-{
-public:
-	virtual ~A() = default;
-
-	virtual void DoSomething() = 0;
-};
-class B
-	: public A
-{
-public:
-	void DoSomething() override
-	{
-		std::cout << "DoSomething::B";
-	}
-};
-
 
 int main()
 {
-	//std::shared_ptr<A> a = std::make_shared<B>();
-	//a->DoSomething();
-
 	CRUD sys("db.txt");
 	sys.Menu();
-	
-
-
 }
