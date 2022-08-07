7b403a2e58fbe7894b16b1f497da303fd6e67d41
Mateusz Gorzelak <mateusz.gorzelak@umlaut.com>
2022-08-07 19:47:06 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,122 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Switch } from 'react-native';
+import { Amplify, PubSub, Auth } from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub';
+import awsexport from './exp/aws-export' 
+import { useState, Component } from 'react';
+
+
+interface MQTTData {
+  [key: string]: any;
+}
+declare type MQTTDatat = MQTTData;
+const publishTopic: string = "esp32-1/sub";
+const subscribeTopic: string = "esp32-1/pub";
+let sdata: MQTTDatat;
+let counter = 0;
+
+
+Amplify.configure(awsexport);
+PubSub.addPluggable(new AWSIoTProvider({
+  aws_pubsub_region: 'eu-west-1',
+  aws_pubsub_endpoint: 'wss://a1tg3e4a8pndpe-ats.iot.eu-west-1.amazonaws.com/mqtt',
+}));
+PubSub.subscribe(subscribeTopic).subscribe({
+  next: data => {
+    console.log('Message received', data)
+    sdata = data;
+  },
+  error: error => console.error(error),
+  complete: () => console.log('Done'),
+});
+
+const shuttersRefreshStatus = () => {
+  const [shuttersStatus, setShuttersStatusText] = useState("");
+  let sD = "spróbuj zaraz...";
+  const onPressBtn = () => {
+    PubSub.publish(publishTopic, { msg: "report"});
+    if(sdata != null && sdata != undefined){
+      let sDb: boolean = sdata["value"]["state"]["reported"]["shuttersDown"];
+      if(sDb != null){
+        if(sDb === true) {
+          sD = "Rolety są opuszczone";
+        } else {
+          sD = "Rolety są podniesione";
+        }
+      }
+    }
+    setShuttersStatusText(sD);
+  };
+
+  return (
+    <View>
+      <Text style={{marginLeft:"10%", backgroundColor:"#CBD4D6", alignSelf:"flex-start"}}>{shuttersStatus}</Text>
+      <Button
+        title="Odśwież status rolet"
+        color="#CBE4D6"
+        onPress={onPressBtn}
+      ></Button>
+    </View>
+  );
+};
+
+
+const lightSensorFE = () => {
+  const [isEnabled, setlSFESwitch] = useState(false);
+  const [lsfeText, setLsfeText] = useState("Sterowanie funkcją czujnika światła");
+  const toggleSwitch = () => {
+    setlSFESwitch(previousState => !previousState);
+    setLsfeText(() => !isEnabled ? "Funkcja czujnika światła włączona" : "Funkcja czujnika światła wyłączona");
+    if(!isEnabled){
+      PubSub.publish(publishTopic, { msg: "lfe_1"});
+    } else {
+      PubSub.publish(publishTopic, { msg: "lfe_0"});
+    }
+    console.log("switch:" + !isEnabled);
+
+  }
+
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text style={{marginLeft:"0%", backgroundColor:"#CBD4D6", alignSelf:"flex-start"}}>{lsfeText}</Text>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      ></Switch>
+    </View>
+  );
+};
 
 export default function App() {
+  console.log("export default function App()");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {shuttersRefreshStatus()}
+      <View style={{flexDirection:"row"}}>
+        <Button
+          title="Up"
+          color="#CBE4D6"
+          onPress={()=>{
+            PubSub.publish(publishTopic, { msg: "roll_up"});
+            counter=counter+1;
+            console.log(counter);
+          }}
+        ></Button>
+        <Button
+          title="Down"
+          color="#CBE4D6"
+          onPress={()=>{
+            PubSub.publish(publishTopic, { msg: "roll_down"});
+            counter=counter+1;
+            console.log(counter);
+          }}
+        ></Button>
+      </View>
       <StatusBar style="auto" />
+      {lightSensorFE()}
     </View>
   );
 }
---
+++ exp/aws-export.js
@@ -0,0 +1,8 @@
+const awsmobile = {  
+    "aws_project_region": 'eu-west-1',
+    "aws_cognito_identity_pool_id": 'eu-west-1:ca81df9e-401f-4549-a8fd-4c6eac9eec1e',
+    "aws_cognito_region": 'eu-west-1',
+    "aws_user_pools_id": "eu-west-1_1u8PjaLa2",
+    "aws_user_pools_web_client_id": "15bus43eq122srhnn844b82u7r"
+  }
+export default awsmobile;
