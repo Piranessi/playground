73c04de36cbe4d4ffa300fd59ce392c85dbddcb8
Mateusz Gorzelak <matgorzelak@gmail.com>
2023-11-14 22:33:37 +0100

    fixes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelloContextProvider } from './components/HelloContext';
+import { AuthProvider } from './components/AuthContext';
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useAuth } from './AuthContext';
 
-function SpotifyAuthorization (){
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+function SpotifyAuthorization() {
+  const { isLoggedIn, login } = useAuth();
 
   useEffect(() => {
     const checkSpotifyLoginStatus = async () => {
-      // Make a request to your server to check if the user is logged in to Spotify
       try {
         const response = await axios.get('http://spotifyorganizer.matgosoft.com/check-login');
         const userIsLoggedIn = response.data.isLoggedIn;
-        setIsLoggedIn(userIsLoggedIn);
+        if (userIsLoggedIn) {
+          login(); // Update the context if the user is logged in
+        }
       } catch (error) {
         console.error('Error:', error);
         // Handle error if needed
@@ -18,35 +20,20 @@ function SpotifyAuthorization (){
     };
 
     checkSpotifyLoginStatus();
-  }, []); // Empty dependencies array to run once on mount
-
-  const redirectToSpotifyLogin = async () => {
-    try {
-      const response = await axios.get('http://spotifyorganizer.matgosoft.com/login');
-      const authorizeURL = response.data.authorizeURL;
-      window.location.href = authorizeURL;
-      console.log("authorizeURL: ", authorizeURL); // DEBUG
-    } catch (error) {
-      console.error('Error:', error);
-      // Handle error if needed
-    }
-  };
-
-  // Assuming you have a button or some UI element to trigger the login
-  const handleLoginButtonClick = () => {
-    if (!isLoggedIn) {
-      redirectToSpotifyLogin();
-    } else {
-      // User is already logged in, handle accordingly
-    }
-  };
+  }, [login]);
 
   return (
     <div className="Login">
-      {/* Your UI elements */}
-      <button onClick={handleLoginButtonClick}>Login to Spotify</button>
+      {isLoggedIn ? (
+        <p>User is logged in!</p>
+      ) : (
+        <>
+          <p>User is not logged in!</p>
+          {/* Your login UI elements */}
+        </>
+      )}
     </div>
   );
-};
+}
 
 export default SpotifyAuthorization;
