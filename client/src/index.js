0f56e83c2d2e25ce1dc3b8a9125d69a6fb92406a
Mateusz Gorzelak <matgorzelak@gmail.com>
2023-11-14 22:41:30 +0100

    fixes

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelloContextProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </HelloContextProvider>
   </React.StrictMode>
 );
