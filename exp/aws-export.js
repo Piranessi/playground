315b889b05af0c989d59109f9463e67a4113c883
Mateusz <14152450+Piranessi@users.noreply.github.com>
2025-06-06 11:45:00 +0200

    Update aws-export.js

--- exp/aws-export.js
+++ exp/aws-export.js
@@ -1,8 +1,8 @@
 const awsmobile = {  
-    "aws_project_region": 'eu-west-1',
-    "aws_cognito_identity_pool_id": 'eu-west-1:ca81df9e-401f-4549-a8fd-4c6eac9eec1e',
-    "aws_cognito_region": 'eu-west-1',
-    "aws_user_pools_id": "eu-west-1_1u8PjaLa2",
-    "aws_user_pools_web_client_id": "15bus43eq122srhnn844b82u7r"
+    "aws_project_region": '-',
+    "aws_cognito_identity_pool_id": '-',
+    "aws_cognito_region": '-',
+    "aws_user_pools_id": "-",
+    "aws_user_pools_web_client_id": "-"
   }
 export default awsmobile;
