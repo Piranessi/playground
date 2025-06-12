18b49a7ffa4675d74613ff3f0654f3020c2a8b10
Mateusz Gorzelak <matgorzelak@gmail.com>
2025-06-12 18:14:02 +0200

    leetcode excercise

--- react_frontend/src/misc/w1.ts
+++ react_frontend/src/misc/w1.ts
@@ -717,3 +717,38 @@ for (const shape of arrShapes) {
 // const shapeExample: Shape = new Shape("Ogólny Kształt");
 // Powyższa linia spowodowałaby błąd TS2511: Cannot create an instance of an abstract class.
 // (Nie można utworzyć instancji klasy abstrakcyjnej).
+
+
+//leetcode task
+// Given a circular array nums, find the maximum absolute difference between adjacent elements.
+// Note: The first and last elements are considered adjacent in a circular array.
+
+// Example 1:
+// Input: nums = [1,2,4]
+// Output: 3
+// Explanation: The maximum absolute difference is between nums[0] and nums[2]: |4 - 1| = 3.
+
+// Example 2:
+// Input: nums = [-5,-10,-5]
+// Output: 5
+// Explanation: The maximum absolute difference is between nums[0] and nums[1]: |-5 - (-10)| = 5.
+
+function maxAdjacentDistance(nums: number[]): number {
+    if (
+        nums.length < 2 
+        || nums.length > 100
+        || nums.some(i => i > 100 || i < -100)
+    ) {
+        console.log(`Incorrect input data.`);
+        return 0;
+    }
+
+    let maxAbsoluteDiff = Math.abs(nums[0] - nums[nums.length-1]);
+    for (let i = 0; i < nums.length-1; i++) {
+        const currentCompare = Math.abs(nums[i] - nums[i+1]);
+        if (currentCompare > maxAbsoluteDiff) {
+            maxAbsoluteDiff = currentCompare;
+        }
+    }
+    return maxAbsoluteDiff;
+}
